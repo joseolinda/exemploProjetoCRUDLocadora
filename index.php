cae5d2d05458ca8b38ee92cbd8715d06583cc0a4
José Olinda <joseolindad@yahoo.com.br>
2019-09-13 11:22:16 -0300

    Adição de outro exemplo de tabela: ator

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 // Importar autoload
 require_once __DIR__ . "/App/autoload.php";
 
-echo '<a href="listar.php?p=filme" >Veja a lista de Filmes</a>';
+echo '<a href="listar.php?p=filme" >Veja a lista de Filmes</a><br/>';
+echo '<a href="listar.php?p=ator" >Veja a lista de Atores</a><br/>';
