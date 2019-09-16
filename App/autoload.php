4c25d434155b53483323a588ee18c2bbfab7ec57
Johnny Ferreira <46564172+johnnyferreiradevweb@users.noreply.github.com>
2019-09-16 20:56:27 -0300

    Atualização autoload

--- App/autoload.php
+++ App/autoload.php
@@ -1,8 +1,13 @@
 <?php
 
+    namespace App;
 
-namespace App;
+    define('__ROOT__', dirname(dirname(__FILE__)) . "/");
 
-spl_autoload_register(function ($class){
-    require_once str_replace( '\\', '/', $class . ".php");
-});
+    spl_autoload_register(function ($class){
+        $class = str_replace( '\\', '/', $class . ".php");
+        $fullClass = __ROOT__. $class;
+        if(file_exists($fullClass)){
+            require_once( $fullClass );
+        }
+    });
